22d0642b0782331d4266237aa47deeb3f57260af
Dexter Chua <dec41@srcf.net>
2020-08-09 11:32:15 +0800

    Fix linter script on sh

--- linter.sh
+++ linter.sh
@@ -5,7 +5,7 @@ set +e
 forbid() {
     # grep returns 1 if there is no match, 0 otherwise
     grep --color=always -n -E -r $1 src
-    if [[ $? -eq 0 ]]; then
+    if [ $? -eq 0 ]; then
         exit 1
     fi
 }

25727e7b14a01fc39118f350f3d7f72cb23c19db
Dexter Chua <dec41@srcf.net>
2020-09-19 13:09:20 +0800

    Add tests for rename admin endpoint
    Fixes #64

--- src/tests/07-endpoint.ts
+++ src/tests/07-endpoint.ts
@@ -1,4 +1,6 @@
 import { startBridge, test, main } from './utils/Bridge';
+import { getMattermostClient } from './utils/Client';
+
 import fetch from 'node-fetch';
 
 test('Start bridge', async t => {
@@ -26,6 +28,69 @@ test('Appservice should reject invalid token', async t => {
     });
 
     t.end();
+});
+
+test('Rename admin endpoint', async t => {
+    const hsToken = main().registration.hs_token;
+    const client = getMattermostClient('admin');
+
+    await fetch(
+        `http://localhost:9995/bridge/rename/matrix_matrix_a/matrix_a?access_token=${hsToken}`,
+        { method: 'POST' },
+    );
+    t.equal((await client.post('/users/usernames', ['matrix_a'])).length, 1);
+    t.deepEqual(await client.post('/users/usernames', ['matrix_matrix_a']), []);
+
+    await fetch(
+        `http://localhost:9995/bridge/rename/matrix_a/matrix_matrix_a?access_token=${hsToken}`,
+        { method: 'POST' },
+    );
+    t.equal(
+        (await client.post('/users/usernames', ['matrix_matrix_a'])).length,
+        1,
+    );
+    t.deepEqual(await client.post('/users/usernames', ['matrix_a']), []);
+
+    t.end();
+});
+
+test('Do not rename if target already exists', async t => {
+    const hsToken = main().registration.hs_token;
+    const client = getMattermostClient('admin');
+    const result = await fetch(
+        `http://localhost:9995/bridge/rename/matrix_matrix_a/matrix_matrix_b?access_token=${hsToken}`,
+        { method: 'POST' },
+    );
+    t.equal(result.status, 500, 'Correct status code 500');
+    t.equal(
+        (await client.post('/users/usernames', ['matrix_matrix_a'])).length,
+        1,
+        'User not renamed',
+    );
+});
+
+test('Do not rename non-puppet users', async t => {
+    const hsToken = main().registration.hs_token;
+    const client = getMattermostClient('admin');
+    const result = await fetch(
+        `http://localhost:9995/bridge/rename/mattermost_a/mattermost_c?access_token=${hsToken}`,
+        { method: 'POST' },
+    );
+    t.equal(result.status, 400, 'Correct status code 400');
+    t.equal(
+        (await client.post('/users/usernames', ['mattermost_a'])).length,
+        1,
+        'User not renamed',
+    );
+});
+
+test('Return correct error when renaming non-existent user', async t => {
+    const hsToken = main().registration.hs_token;
+    const result = await fetch(
+        `http://localhost:9995/bridge/rename/nonexistent/newname?access_token=${hsToken}`,
+        { method: 'POST' },
+    );
+    t.equal(result.status, 400, 'Correct status code 400');
 });
 
 test('Status does not need access token', async t => {
